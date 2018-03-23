377645e3a713876e6e4585432711fc4fbb07be07
snihar2 <36260910+snihar2@users.noreply.github.com>
2018-03-23 19:27:57 +0100

    Update gamma1

--- gamme1.m
+++ gamme1.m
@@ -1,4 +1,7 @@
-##travail Séance 2 2018 ECAM
+## Copyright (C) 2018 harol
+## Author: harol <harol@PC-HAROLD>
+## Created: 2018-03-23
+## travail Séance 1
 
 
 #vitesse allant de 0 à la vitesse de la lumière
