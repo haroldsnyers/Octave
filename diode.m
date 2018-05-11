3afeb675588bed838260b51c8a402156687f66a1
snihar2 <36260910+snihar2@users.noreply.github.com>
2018-05-11 15:41:24 +0200

    update diode

--- diode.m
+++ diode.m
@@ -1,3 +1,8 @@
+## Copyright (C) 2018 harol
+## Author: harol <harol@PC-HAROLD>
+## Created: 2018-02-23
+## Séance 1, exercices 1, diode
+
 function [Id] = diode (Is,T,Vak)
   k = 1.38e-23;
   q = 1.6e-19;
